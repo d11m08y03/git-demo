18988fb4d1b1fe1c863bb03e04a200e2ee5445a0
Zakariyya Kurmally <zakariyyakurmally@gmail.com>
2026-02-05 20:27:02 +0400

    More hello world

--- main.c
+++ main.c
@@ -2,5 +2,23 @@
 
 int main(void) {
 	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
+	puts("Hello World");
 	return 0;
 }
